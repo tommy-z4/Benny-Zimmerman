c91a5378c296fbd05f521ffa1a88976f8437e520
tommy-z4 <zimmermantommy4@gmail.com>
2025-04-23 21:34:14 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,34 +1,36 @@
-const carouselItems = document.querySelectorAll(".carousel_item");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
+const carousel = document.getElementById("carousel");
+    const items = document.querySelectorAll(".carousel_item");
+    const nextBtn = document.getElementById("next");
+    const prevBtn = document.getElementById("prev");
+    let index = 0;
+    let interval;
 
-// It will be used to shift the items  by incrementing its
-// value every 2 seconds and multiplying it by 100
-let i = 1;
-
-// Running the condition every 2 seconds using setInterval
-setInterval(() => {
-  // Accessing All the carousel Items
-  Array.from(carouselItems).forEach((item, index) => {
-    // Checking if the value of i is less than the
-    // number of carousel items, if it is true,
-    // move all the carousel items 100% towards left
-    if (i < carouselItems.length) {
-      item.style.transform = `translateX(-${i * 100}%)`;
+    function updateSlide(i) {
+      index = (i + items.length) % items.length;
+      carousel.style.transform = `translateX(-${index * 100}%)`;
     }
-  });
 
-  // If the the value of i is less than the number of
-  // Carousel items, then increment the value of i by 1
-  // Otherwise set the value of i to 0 if it becomes greater
-  // than the number of carousel items
-  if (i < carouselItems.length) {
-    i++;
-  } else {
-    i = 0;
-  }
-}, 3500);
+    function startAutoSlide() {
+      interval = setInterval(() => updateSlide(index + 1), 4500);
+    }
 
+    function stopAutoSlide() {
+      clearInterval(interval);
+      startAutoSlide(); // מחזיר את המעבר האוטומטי אחרי פעולה ידנית
+    }
+
+    nextBtn.addEventListener("click", () => {
+      updateSlide(index + 1);
+      stopAutoSlide();
+    });
+
+    prevBtn.addEventListener("click", () => {
+      updateSlide(index - 1);
+      stopAutoSlide();
+    });
+
+    // התחלת מעבר אוטומטי
+    startAutoSlide();
 
 
 
@@ -56,6 +58,9 @@ document.querySelector('.mainstop').addEventListener('click', function() {
       this.classList.remove('active');
   }
 });
+
+
+
 
 
 
